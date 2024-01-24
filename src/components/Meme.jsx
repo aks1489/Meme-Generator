e72fb11ca19c5316909ab79e1a250455059f2572
Akash Sarkar <akash.sarkar1489@outlook.com>
2024-01-24 13:02:46 +0530

    update meme component

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,15 +1,13 @@
 export default function Meme(){
     return(
-        <form className="form" action="">
-            <div className="form-input-container">
-                <label htmlFor="top-text">Top Text</label>
-                <input className="form-imput-box" id="top-text" name="top-text" type="text" value="Shut up"/>
-                <label htmlFor="bottom-text">Bottom Text</label>
-                <input className="form-input-box" id="bottom-text" name="bottom-text" type="text" value="And take my money" />
-            </div>
-            <div className="form-submit">
+        <div className="main">
+            <form className="form" action="">
+                {/* <label htmlFor="top-text">Top Text</label> */}
+                <input className="form-input" id="top-text" name="top-text" type="text" placeholder="Shut up" />
+                {/* <label htmlFor="bottom-text">Bottom Text</label> */}
+                <input className="form-input" id="bottom-text" name="bottom-text" type="text" placeholder="And take my money" />
                 <button type="submit" className="form-submit-button">Get a new meme image 🖼️</button>
-            </div>
-        </form>
+            </form>
+        </div>
     )
 }
