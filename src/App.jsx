f770b7afdce7e3dc6dd2081b0b18068687368af3
Akash Sarkar <akash.sarkar1489@outlook.com>
2024-01-24 11:56:17 +0530

    Header & Header-Styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css'
+import Header from './components/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
+      <Header />
     </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+export default function Header(){
+    return(
+        <header className="nav-bar">
+            <nav className="nav">
+                <div className="nav-title">
+                    <img src="/images/troll-face.png" alt="" />
+                    <h1>Meme Generator</h1>
+                </div>
+                <div className="nav-text">
+                    <p className="nav-text-item">React Course - Project 3</p>
+                </div>
+            </nav>
+        </header>
+    )
+}
