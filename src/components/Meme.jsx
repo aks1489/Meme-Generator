a502494758513222e772882c49161ea3b60a554d
Akash Sarkar <akash.sarkar1489@outlook.com>
2024-01-25 19:18:12 +0530

    memedata testing

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,7 +1,20 @@
+import memedata from "./memedata"
+
 export default function Meme(){
+    function getMemeImage(){
+        if(memedata.success === true){
+            const memesArray = memedata.data.memes;
+            const randomNumber = Math.floor(Math.random() * memesArray.length)
+            const url = memesArray[randomNumber].url
+            return console.log(url)
+        }else{
+            console.log("not connected with database")
+        }
+    }
+    
     return(
         <div className="main">
-            <form className="form" action="#">
+            <div className="form" action="#">
                 <div className="form-input-container">
                     <div className="input-box">
                         <label htmlFor="top-text" className="form-lable">Top Text</label>
@@ -12,8 +25,8 @@ export default function Meme(){
                         <input className="form-input"  id="bottom-text" name="bottom-text" type="text" placeholder="And take my money" />
                     </div>
                 </div>
-                <button type="submit"  className="form-submit-button">Get a new meme image 🖼️</button>
-            </form>
+                <button type="submit" onClick={getMemeImage}  className="form-submit-button">Get a new meme image 🖼️</button>
+            </div>
         </div>
     )
 }
