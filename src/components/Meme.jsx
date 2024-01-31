6a33cb527863337fbbe990930b1c9bd1e6b79eb0
Akash Sarkar <akash.sarkar1489@outlook.com>
2024-01-31 20:10:20 +0530

    dynamic data update to html

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -8,7 +8,7 @@ export default function Meme(){
         randomImg: "http://i.imgflip.com/1bij.jpg"
     })
     // eslint-disable-next-line no-unused-vars
-    const [memeUrl,setMemeUrl] = useState(memedata)
+    const [memeUrl] = useState(memedata)
     function getMemeUrl(){
         if(memeUrl.success === true){
             const memesArray = memeUrl.data.memes;
@@ -23,26 +23,33 @@ export default function Meme(){
             console.log("not connected with database")
         }
     }
-    
+
+    function handleChange(event){
+        const {name, value} = event.target
+        setMeme(prvData => ({
+            ...prvData,
+            [name] : value
+        }))
+    }
     return(
         <div className="main">
             <div className="form" action="#">
                 <div className="form-input-container">
                     <div className="input-box">
                         <label htmlFor="top-text" className="form-lable">Top Text</label>
-                        <input className="form-input" id="top-text" name="top-text" type="text" placeholder="Shut up" />
+                        <input className="form-input" id="topText" name="topText" type="text" placeholder="Shut up" value={meme.topText} onChange={handleChange}/>
                     </div>
                     <div className="input-box">
                         <label htmlFor="bottom-text" className="form-lable">Bottom Text</label>
-                        <input className="form-input"  id="bottom-text" name="bottom-text" type="text" placeholder="And take my money" />
+                        <input className="form-input"  id="bottomText" name="bottomText" type="text" placeholder="And take my money" value={meme.bottomText} onChange={handleChange}/>
                     </div>
                 </div>
                 <button type="submit" onClick={getMemeUrl}  className="form-submit-button">Get a new meme image 🖼️</button>
             </div>
             <div className="meme-img-section">
                 <img src={meme.randomImg} alt="" className="meme-img" />
-                <h2 className="imgText top">Top Text</h2>
-                <h2 className="imgText bottome">Bottome Text</h2>
+                <h2 className="imgText top">{meme.topText}</h2>
+                <h2 className="imgText bottome">{meme.bottomText}</h2>
             </div>
         </div>
     )
